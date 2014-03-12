0dec1313f369bfaedb9f9fcee8f733ae238ad435
Richard Davis <rdavis.bacs@gmail.com>
2014-03-12 01:43:46 +0000

    added dotenv

--- pi_mail.rb
+++ pi_mail.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 require 'net/smtp'
+require 'logger'
+#require 'pry'
+require 'dotenv'
+Dotenv.load
+
+@log = Logger.new(File.expand_path('../pi_mail.log', __FILE__))
+@log.debug "pimail started"
 
 def send_email(opts={})
   msg = <<END_OF_MESSAGE
@@ -12,8 +19,16 @@ END_OF_MESSAGE
 
   email = Net::SMTP.new(opts[:server], opts[:port])
   email.enable_starttls
-  email.start(opts[:from_domain], opts[:from], opts[:password], :login) do |smtp|
-    smtp.send_message(msg, opts[:from], opts[:to])
+
+  begin
+    email.start(opts[:from_domain], opts[:from], opts[:password], :login) do |smtp|
+      smtp.send_message(msg, opts[:from], opts[:to])
+    end
+    @log.debug "email delivery to #{opts[:to].inspect}: successful"
+  rescue => e
+    @log.error "email delivery to #{opts[:to].inspect}: failed"
+    @log.error "error: #{e}"
+    #@log.debug "opts: #{opts}"
   end
 end
 
@@ -34,3 +49,5 @@ if __FILE__ == $0
 
   send_email(opts)
 end
+
+@log.debug "pimail exiting"
